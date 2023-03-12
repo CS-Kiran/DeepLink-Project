8bb688a3ea4291222e3f4e0955409883c591d74f
Code@Kiran <109469540+CS-Kiran@users.noreply.github.com>
2023-03-12 16:54:00 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,3 @@
 <?php
-$conn = pg_connect("host=localhost dbname=deeplink user=postgres password=kiran") or die("Unable to connect to database");
+$conn = pg_connect("host=localhost dbname=deeplink user=postgres password=") or die("Unable to connect to database");
 ?>
